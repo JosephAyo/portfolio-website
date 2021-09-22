a709b795e35fe010074edfe7d31c6b325906f80c
JosephAyo <josephayo110@gmail.com>
2021-09-23 00:18:20 +0100

    changed side panel for mobile view to hide with click

--- src/components/HeaderPanel/HeaderPanel.jsx
+++ src/components/HeaderPanel/HeaderPanel.jsx
@@ -6,8 +6,7 @@ import HeaderPanelItems from "../HeaderPanelItems/HeaderPanelItems";
 
 const sections = ["Home", "Skills", "Projects", "Contact"];
 const HeaderPanel = (props) => {
-  const [opaquePanel, setOpaquePanel] = useState(true);
-  const [clickedToScroll, setClickedToScroll] = useState(false);
+  const [showPanel, setShowPanel] = useState(true);
   const { displayMode, setDisplayMode } = useContext(DisplayContext);
 
   useEffect(() => {
@@ -18,12 +17,10 @@ const HeaderPanel = (props) => {
   });
 
   const scrollHandler = () => {
-    if (window.pageYOffset > 40 && !clickedToScroll) {
-      setOpaquePanel(false);
-    } else if (window.pageYOffset > 40) {
-      setOpaquePanel(false);
-    } else {
-      setOpaquePanel(true);
+    if (window.pageYOffset > 40) {
+      setShowPanel(false);
+    } else if (window.pageYOffset === 0) {
+      setShowPanel(true);
     }
   };
 
@@ -41,66 +38,85 @@ const HeaderPanel = (props) => {
       left: 0,
       behavior: "smooth",
     });
-    setClickedToScroll(true);
-    setPanelOpaqueTemp(sectionIndex);
-  };
-
-  const setPanelOpaqueTemp = (sectionIndex) => {
-    setOpaquePanel(true);
-    if (sectionIndex !== 0)
-      setTimeout(() => {
-        setOpaquePanel(false);
-      }, 1000);
-    setClickedToScroll(false);
   };
 
   return (
-    <div
-      className={opaquePanel ? "header-panel" : "header-panel transparent"}
-      onMouseEnter={() => setOpaquePanel(true)}
-      onMouseLeave={() => setOpaquePanel(false)}
-    >
-      <div className="row  align-items-center panel">
-        <div className="col-md-7 profile">
-          <p>Ayo</p>
-        </div>
-        {sections.map((section, index) => (
-          <HeaderPanelItems
-            itemName={section}
-            sectionIndex={index}
-            onClickItem={() => headerItemClickHandler(index)}
-          />
-        ))}
-        <div
-          className="col-md-1 switch-tag"
-          onClick={() => setDisplayModeHandler()}
-        >
-          <p>
-            <i
-              style={{ fontSize: "14px" }}
-              class={displayMode === "dark" ? "far fa-moon" : "far fa-sun"}
-              aria-hidden="true"
-            ></i>
-          </p>
+    <div className="main-panel">
+      <div
+        className={showPanel ? "header-panel" : "header-panel transparent"}
+        onMouseEnter={() => setShowPanel(true)}
+        onMouseLeave={() => setShowPanel(false)}
+      >
+        <div className="row  align-items-center panel">
+          <div className="col-md-7 profile">
+            <p>Ayo</p>
+          </div>
+          {sections.map((section, index) => (
+            <HeaderPanelItems
+              itemName={section}
+              sectionIndex={index}
+              onClickItem={() => headerItemClickHandler(index)}
+              key={index}
+            />
+          ))}
+          <div
+            className="col-md-1 switch-tag"
+            onClick={() => setDisplayModeHandler()}
+          >
+            <p>
+              <i
+                style={{ fontSize: "14px" }}
+                className={
+                  displayMode === "dark" ? "far fa-moon" : "far fa-sun"
+                }
+                aria-hidden="true"
+              ></i>
+            </p>
+          </div>
         </div>
       </div>
-      {/* <div className="panel">
-        {sections.map((section, index) => (
-          <HeaderPanelItems itemName={section} sectionIndex={index} />
-        ))}
-        <div
-          className="switch-tag"
-          onClick={() => setDisplayModeHandler()}
-        >
-          <p>
-            <i
-              style={{ fontSize: "14px" }}
-              class={displayMode === "dark" ? "far fa-moon" : "far fa-sun"}
-              aria-hidden="true"
-            ></i>
-          </p>
+
+      <div
+        className={showPanel ? "mobile-panel show" : "mobile-panel"}
+        onMouseEnter={() => setShowPanel(true)}
+        onMouseLeave={() => setShowPanel(false)}
+      >
+        <div className="panel">
+          {sections.map((section, index) => (
+            <HeaderPanelItems
+              itemName={section}
+              sectionIndex={index}
+              onClickItem={() => headerItemClickHandler(index)}
+              key={index}
+            />
+          ))}
+          <div
+            className="col-md-1 switch-tag"
+            onClick={() => setDisplayModeHandler()}
+          >
+            <p>
+              <i
+                style={{ fontSize: "14px" }}
+                className={
+                  displayMode === "dark" ? "far fa-moon" : "far fa-sun"
+                }
+                aria-hidden="true"
+              ></i>
+            </p>
+          </div>
         </div>
-      </div> */}
+        <div className="panel-control">
+          <div onClick={() => setShowPanel(!showPanel)}>
+            <p>
+              <i
+                style={{ fontSize: "14px" }}
+                className={showPanel ? "	fa fa-close" : "fas fa-angle-right"}
+                aria-hidden="true"
+              ></i>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
